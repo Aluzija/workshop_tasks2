7cf53b1c8a8e44e23ebebcf4050d68443a6929e4
Paulina Sorys <poland_paulina@yahoo.pl>
2018-05-07 20:38:54 +0200

    New task can be added, repair TaskController tests

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -11,17 +11,22 @@ RSpec.describe TasksController, type: :controller do
 
   describe 'POST tasks' do
     context "for no task attributes given" do
-      it 'renders new template' do
-        post :create, params: { task: { foo: 'bar' } }
-        expect(response).to be_success
-        assert_template 'tasks/new'
+      it 'does not create new task and renders new template' do
+        expect {
+          post :create, params: { task: { foo: 'bar' } }
+        }.not_to change(Task, :count)
+        expect(response).to render_template(:new)
+        expect(response).not_to redirect_to tasks_url
       end
     end
 
     context "for task attributes given" do
-      it 'renders new template' do
-        post :create, params: { task: { title: 'foo', description: 'bar' } }
+      it 'creates new task and redirects to index view' do
+        expect {
+          post :create, params: { task: { title: 'foo', description: 'bar' } }
+        }.to change(Task, :count).by 1
         expect(response).to redirect_to tasks_url
+        expect(response).not_to render_template(:new)
         expect(flash[:notice]).to be_present
       end
     end
