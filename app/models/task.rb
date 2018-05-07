333f333e5cd2da6d444efb048e03dd20f3151301
Paulina Sorys <poland_paulina@yahoo.pl>
2018-05-07 21:59:18 +0200

    Add deadline to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+  validate :deadline_is_date?
+
+  private
+
+  def deadline_is_date?
+    if !deadline.is_a?(Date)
+      errors.add(:deadline, 'must be in a format of YYYY-MM-DD')
+    end
+  end
 end
